347ff9efcb6dba5ea73754f3088bd3bbf387213d
Matt Manhardt <mattmanhardt@gmail.com>
2020-07-25 22:00:40 -0500

    add reset password function

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,18 +2,18 @@
   <v-container fluid>
     <v-row justify="center">
       <v-col cols="11" sm="6" lg="4" align="center" justify="center">
-        <v-card v-if="showLoginForm" :elevation="$vuetify.breakpoint.smAndDown ? 0 : 4"  :class="$vuetify.breakpoint.smAndDown ? '': 'login-card'">
-        <h1>Welcome Back</h1>
-          <v-text-field label="Email" v-model.trim="loginForm.email" :error-messages="emailErrors"  @input="$v.loginForm.email.$touch()" @blur="$v.loginForm.email.$touch()"></v-text-field>
-          <v-text-field label="Password" type="password" v-model.trim="loginForm.password" :error-messages="passwordErrors"  @input="$v.loginForm.password.$touch()" @blur="$v.loginForm.password.$touch()"></v-text-field>
-        <v-btn @click="login" color="primary">Log In</v-btn>
+        <v-card v-if="formType === LOGIN_FORM_TYPE" :elevation="$vuetify.breakpoint.smAndDown ? 0 : 4"  :class="$vuetify.breakpoint.smAndDown ? '': 'login-card'">
+          <h1>Welcome Back</h1>
+            <v-text-field label="Email" v-model.trim="loginForm.email" :error-messages="emailErrors"  @input="$v.loginForm.email.$touch()" @blur="$v.loginForm.email.$touch()"></v-text-field>
+            <v-text-field label="Password" type="password" v-model.trim="loginForm.password" :error-messages="passwordErrors"  @input="$v.loginForm.password.$touch()" @blur="$v.loginForm.password.$touch()"></v-text-field>
+          <v-btn @click="login" color="primary">Log In</v-btn>
 
-        <div class="extras">
-          <a :style="{ marginRight: '1rem' }">Forgot Password</a>
-          <a @click="toggleForm">Create an Account</a>
-        </div>
+          <div class="extras">
+            <a :style="{ marginRight: '1rem' }" @click="toggleForm(PASSWORD_RESET_FORM_TYPE)">Forgot Password</a>
+            <a @click="toggleForm(SIGNUP_FORM_TYPE)">Create an Account</a>
+          </div>
         </v-card>
-        <v-card v-else class="login-card">
+        <v-card v-else-if="formType === SIGNUP_FORM_TYPE" class="login-card">
           <h1>Get Started</h1>
           <v-text-field label="First Name" v-model.trim="signupForm.firstName" :error-messages="firstNameErrors" @input="$v.signupForm.firstName.$touch()" @blur="$v.signupForm.firstName.$touch()"></v-text-field>
           <v-text-field label="Last Name" v-model.trim="signupForm.lastName" :error-messages="lastNameErrors"  @input="$v.signupForm.lastName.$touch()" @blur="$v.signupForm.lastName.$touch()"></v-text-field>
@@ -22,11 +22,20 @@
           <v-text-field label="Confirm Password" type="password" v-model.trim="signupForm.password2" :error-messages="password2Errors"  @input="$v.signupForm.password2.$touch()" @blur="$v.signupForm.password2.$touch()"></v-text-field>
           <v-btn @click="signup" color="primary">Sign Up</v-btn>
           <div class="extras">
-            <a @click="toggleForm">Back to Log In</a>
+            <a @click="toggleForm(LOGIN_FORM_TYPE)">Back to Log In</a>
+          </div>
+        </v-card>
+        <v-card v-else class="login-card">
+          <h1>Password Reset</h1>
+          <v-text-field label="Email" v-model.trim="passwordReset.email" :error-messages="emailErrors"  @input="$v.passwordReset.email.$touch()" @blur="$v.passwordReset.email.$touch()"></v-text-field>
+          <v-btn @click="sendResetEmail" color="primary">Reset</v-btn>
+          <div class="extras">
+            <a @click="toggleForm(LOGIN_FORM_TYPE)">Back to Log In</a>
           </div>
         </v-card>
       </v-col>
     </v-row>
+    <v-snackbar v-model="snackbar">{{snackbarMessage}}</v-snackbar>
   </v-container>
 </template>
 
@@ -35,8 +44,17 @@ import { auth, usersCollection } from '../firebaseConfig.js';
 import { validationMixin } from 'vuelidate';
 import { required, email, minLength, sameAs } from 'vuelidate/lib/validators'
 
+const LOGIN_FORM_TYPE = 'login';
+const SIGNUP_FORM_TYPE = 'signup';
+const PASSWORD_RESET_FORM_TYPE = 'passwordReset';
+
 export default {
   mixins: [validationMixin],
+  created() {
+    this.LOGIN_FORM_TYPE = LOGIN_FORM_TYPE;
+    this.SIGNUP_FORM_TYPE = SIGNUP_FORM_TYPE;
+    this.PASSWORD_RESET_FORM_TYPE = PASSWORD_RESET_FORM_TYPE;
+  },
   data() {
     return {
       loginForm: {
@@ -51,7 +69,12 @@ export default {
         password: '',
         password2: ''
       },
-      showLoginForm: true,
+      passwordReset: {
+        email: '',
+      },
+      formType: 'login',
+      snackbar: false,
+      snackbarMessage: '',
     }
   },
   computed: {
@@ -69,7 +92,17 @@ export default {
     },
     emailErrors () {
       const errors = []
-      const emailField = this.$v[this.showLoginForm ? 'loginForm' : 'signupForm'].email;
+      let emailField;
+      if (this.formType === LOGIN_FORM_TYPE) {
+        emailField = this.$v.loginForm.email;
+      }
+      if (this.formType === SIGNUP_FORM_TYPE) {
+        emailField = this.$v.signupForm.email;
+      }
+      if (this.formType === PASSWORD_RESET_FORM_TYPE) {
+        emailField = this.$v.passwordReset.email;
+      }
+
       if (!emailField.$dirty) return errors
       !emailField.required && errors.push('An email is required!')
       !emailField.email && errors.push('A valid email is required!')
@@ -77,10 +110,22 @@ export default {
     },
     passwordErrors () {
       const errors = []
-      const passwordField = this.$v[this.showLoginForm ? 'loginForm' : 'signupForm'].password;
-      if (!passwordField.$dirty) return errors
+      let passwordField;
+      if (this.formType === LOGIN_FORM_TYPE) {
+        passwordField = this.$v.loginForm.password;
+      }
+      if (this.formType === SIGNUP_FORM_TYPE) {
+        passwordField = this.$v.signupForm.password;
+      }
+      if (this.formType === PASSWORD_RESET_FORM_TYPE) {
+        passwordField = this.$v.passwordReset.password;
+      }
+
+      if (!passwordField.$dirty) {
+        return errors
+      }
       !passwordField.required && errors.push('A password is required!')
-      if (!this.showLoginForm) {
+      if (!this.formType) {
         !passwordField.minLength && errors.push('Password must be at least 8 characters long!')
         !passwordField.containsUppercase && errors.push('Password must contain an upper case letter!')
         !passwordField.containsLowercase && errors.push('Password must contain a lower case letter!')
@@ -99,54 +144,71 @@ export default {
     },
   },
   validations() {
-    return this.showLoginForm ? {
-      loginForm: {
-        email: {
-          required,
-          email,
-        },
-        password: {
-          required,
-        },
-      }
-    } : {
-      signupForm: {
-        firstName: {
-          required,
-        },
-        lastName: {
-          required,
-        },
-        email: {
-          required,
-          email,
-        },
-        password: {
-          required,
-          minLength: minLength(8),
-          containsUppercase: function(value) {
-            return /[A-Z]/.test(value)
+    // TODO form type swtich
+    if (this.formType === LOGIN_FORM_TYPE) {
+      return {
+        loginForm: {
+          email: {
+            required,
+            email,
           },
-          containsLowercase: function(value) {
-            return /[a-z]/.test(value)
+          password: {
+            required,
           },
-          containsNumber: function(value) {
-            return /[0-9]/.test(value)
-          },
-          containsSpecial: function(value) {
-            return /[#?!@$%^&*-]/.test(value)
-          }
-        },
-        password2: {
-          required,
-          sameAsPassword: sameAs('password')
         }
       }
     }
+    if (this.formType === SIGNUP_FORM_TYPE) {
+      return {
+        signupForm: {
+          firstName: {
+            required,
+          },
+          lastName: {
+            required,
+          },
+          email: {
+            required,
+            email,
+          },
+          password: {
+            required,
+            minLength: minLength(8),
+            containsUppercase: function(value) {
+              return /[A-Z]/.test(value)
+            },
+            containsLowercase: function(value) {
+              return /[a-z]/.test(value)
+            },
+            containsNumber: function(value) {
+              return /[0-9]/.test(value)
+            },
+            containsSpecial: function(value) {
+              return /[#?!@$%^&*-]/.test(value)
+            }
+          },
+          password2: {
+            required,
+            sameAsPassword: sameAs('password')
+          }
+        }
+      }
+    }
+    if (this.formType === PASSWORD_RESET_FORM_TYPE) {
+      return {
+        passwordReset: {
+          email: {
+            required,
+            email,
+          },
+        }
+      }
+    }
+    return {};
   },
   methods: {
-    toggleForm() {
-      this.showLoginForm = !this.showLoginForm;
+    toggleForm(type) {
+      this.formType = type;
     },
     login() {
       auth.signInWithEmailAndPassword(this.loginForm.email, this.loginForm.password).then(user => {
@@ -176,6 +238,16 @@ export default {
           console.log(err)
         });
     },
+    sendResetEmail() {
+      auth.sendPasswordResetEmail(this.passwordReset.email).then(() => {
+        this.snackbar = true;
+        this.snackbarMessage = 'Password reset email sent!';
+        this.formType = LOGIN_FORM_TYPE;
+      }).catch(function(error) {
+        console.log(error);
+        // An error happened.
+      });
+    }
   }
 }
 </script>
